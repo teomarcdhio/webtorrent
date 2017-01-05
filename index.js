d183f527073ae85935cc75b1f9ba36f4b0229d88
Matteo Marcolini <matteo@MMarcolini700-DT.localdomain>
2017-01-05 16:01:20 +0000

    Structuring the download section

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 var WebTorrent = require('webtorrent');
-
+var express = require('express');
+var app = express();
 var client = new WebTorrent();
+var fs = require('fs-extra'); // this includes both fs and fs-extra
 
-var magnetURI = 'torrents/debian-8.6.0-amd64-netinst.iso.torrent';
-var magnetURI2 = 'torrents/debian-mac-8.6.0-amd64-netinst.iso.torrent';
+//torrent file path - magnet uri - http/https url to a torrent file
+var origin = 'torrents/Sintel.2010.x264-VODO.torrent';
+//destination folder for the current torrent
+var destFolder = 'downloads';
 
-client.add(magnetURI, { path: 'donwloads' }, function (torrent) {
-  torrent.once('progress',function(){
-    console.log('torretn downloading');
+client.add(origin, { path: destFolder }, function (torrent) {
+  //while donwloading display the time left, speed and overall progress
+  torrent.on('download',function(bytes){
+    var timeLeft = Math.round((torrent.timeRemaining / 1000),2);
+    console.log(timeLeft + ' seconds left');
+    var speedKb =  Math.round(((1/1024) * torrent.downloadSpeed), 2) ;
+    console.log(speedKb + ' KBps');
+    var progress = (torrent.progress.toFixed(2))*100;
+    console.log(progress + '% completed');
   });
-  torrent.once('done', function () {
+  //once completed, notify of completion
+  torrent.on('done', function () {
     console.log('torrent download finished');
+
+    //delete the content of the donwloads folder -- dev purpose only
+    fs.emptyDir('downloads', function(err){
+      if (!err)
+      console.log('downloads folder removed');
+      else
+      console.log(err);
+    });
   });
 
 
